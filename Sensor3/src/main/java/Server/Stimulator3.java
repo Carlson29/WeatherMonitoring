86ad404edf155deca977330bbf99bab2c2b294c3
Carlson29 <kometacarlson@gmail.com>
2025-04-23 21:39:54 +0100

    creating sensor 3

---
+++ Sensor3/src/main/java/Server/Stimulator3.java
@@ -0,0 +1,55 @@
+package Server;
+
+import business.Sensor3;
+import business.Weather;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Stimulator3 {
+    public static void main(String[] args) {
+        try {
+
+            Sensor3 sensor3 = new Sensor3();
+            int portNum = 12345;
+
+
+            startRegistry(portNum);
+            String registryURL = "rmi://localhost:" + portNum;
+            String weatherObjectLabel = "/Sensor3";
+            Naming.rebind(registryURL + weatherObjectLabel + "", sensor3);
+            while (true) {
+                Weather w = sensor3.getWeather();
+                sensor3.addWeather(w);
+                Thread.sleep(200);
+            }
+        } catch (
+                RemoteException ex) {
+            System.out.println("Error occurred when making remote objects: " + ex.getMessage());
+        } catch (MalformedURLException ex) {
+            System.out.println("Error occurred when storing Service Objects at specified URL: " + ex.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void startRegistry(int RMIPortNum) throws RemoteException {
+        try {
+            // Try to get the registry at a specific port number
+            // If there is no registry started on that port, an exception will be thrown
+            Registry registry = LocateRegistry.getRegistry(RMIPortNum);
+
+            registry.list();
+        } catch (RemoteException ex) {
+            // No valid registry at that port.
+            System.out.println("RMI registry cannot be located at port " + RMIPortNum);
+
+            // Create a registry on the given port number
+            LocateRegistry.createRegistry(RMIPortNum);
+            System.out.println("RMI registry created at port " + RMIPortNum);
+        }
+    }
+}
