074cd73ac3efa7e794be04c60d1b944d742ec051
Mercy <d00251752@student.dkit.ie>
2025-04-26 18:15:59 +0100

    add 2 Aggregator

---
+++ Agg1/src/main/java/Interface/SensorInterface.java
@@ -0,0 +1,11 @@
+package Interface;
+
+import business.Weather;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface SensorInterface extends Remote {
+    public List<Weather> getWeatherList() throws RemoteException;
+}
